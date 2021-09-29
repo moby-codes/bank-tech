83bc961e7b7947d5f3f85a1c15cb5f5fc080a517
moby-codes <mubashar.jaffri@gmail.com>
2021-09-29 12:02:33 +0100

    refactored test file to use let statement for new account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,21 +1,18 @@
 require 'account'
 
 describe 'account' do
-  
+  let(:account) { Account.new }
   
   it 'creates a new account' do
-    account = Account.new
     expect(account.balance).to eq(0)
   end
 
   it 'allows you to add money to the account balance' do
-    account = Account.new
     account.credit(500)
     expect(account.balance).to eq(500)
   end
 
   it 'allows you to withdraw money from the account balance' do
-    account = Account.new
     account.credit(500)
     account.debit(450)
     expect(account.balance).to eq(50)
